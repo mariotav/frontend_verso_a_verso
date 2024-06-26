1c16e3732bdbd2e67f243caee8fb63a2ec3fac85
mariotave <mario.taveira@escolar.ifrn.edu.br>
2024-06-26 19:53:37 -0300

    integração backend

--- js/formulas.js
+++ js/formulas.js
@@ -1,9 +1,23 @@
 const fisica = document.querySelector("#formsfisica");
-fisica.addEventListener ("click", () => {
-     fetch('http://localhost:3000/formulas') .then(response => { 
-        // Verifica se a resposta é bem-sucedida 
-        if (!response.ok) { throw new Error('Network response was not ok ' + response.statusText); } 
-        return response.json();}) .then(data => {
-            console.log(data)
-        });
+const disciplinas = fisica.getAttribute("disciplinas");
+
+console.log(fisica);
+console.log(disciplinas);
+
+
+fisica.addEventListener("click", () => {
+    fetch(`http://localhost:3000/formulas/${disciplinas}`).then(response => {
+         //Verifica se a resposta é bem-sucedida 
+        if (!response.ok) { throw new Error('Network response was not ok ' + response.statusText); }
+        console.log("sucesso");
+        return response.json();
+    }).then(data => {
+        let ul = document.querySelector("#listaDisciplinas");
+        data.forEach(element => {
+            let li = document.createElement("li");
+            li.classList.add("li-disciplina");
+           //ul.appendChild(li);
+       });
+        console.log(data)
+    });
 });
